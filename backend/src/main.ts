b59f056f2e6d0a7919ee7b5b577fb6a61e06b261
Martin Kagamino Lehoux <martin@lehoux.net>
2022-07-16 13:31:22 +0200

    feat: Enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: "50mb" }));
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
