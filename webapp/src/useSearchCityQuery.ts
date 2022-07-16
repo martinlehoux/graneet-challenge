0a154d6fcbed8c4e17fc3db4cdc5113f70f1a748
Martin Kagamino Lehoux <martin@lehoux.net>
2022-07-16 17:13:53 +0200

    fix: Disable https

--- webapp/src/useSearchCityQuery.ts
+++ webapp/src/useSearchCityQuery.ts
@@ -4,7 +4,7 @@ import { useDebounce } from "./useDebounce";
 
 export function useSearchCityQuery(query: string) {
   const debounced = useDebounce(query, 300);
-  const url = new URL("api/city/search", window.location.host);
+  const url = new URL("api/city/search", window.location.href);
   url.searchParams.append("q", debounced);
   return useQuery<SearchResult>(
     ["city/search", debounced],
